26b2ca34fa204bdfd28afb167ba10baacf7713a5
farzah14 <farzahwalker46@gmail.com>
2026-05-23 16:48:21 +0700

    Update : Adding Docstring With Google Style.

--- src/extract.py
+++ src/extract.py
@@ -12,8 +12,17 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(filename)s - %(lineno)d - %(levelname)s - %(message)s"
 )
 
-def download_dataset(dataset:str, output_dir: Path | None = None, force_download: bool = False):
-    """Download Dataset From Kaggle to target directory"""
+def download_dataset(dataset:str, output_dir: Path, force_download: bool = False):
+    """Download Dataset From Kaggle to target directory
+    
+    Args:
+        dataset (str): The name of the dataset to download.
+        output_dir (Path | None): The path to the directory where the dataset will be downloaded.
+        force_download (bool): Whether to force the download of the dataset. When the dataset exists
+    
+    Returns:
+        Path: The path to the downloaded dataset.
+    """
     logger.info(f"🔄 Download Dataset From Kaggle")
     
     # Check path directory "data". If there is any, the data will be saved there.
@@ -27,7 +36,15 @@ def download_dataset(dataset:str, output_dir: Path | None = None, force_download
     return path_dataset
 
 def find_first_csv(dataset_path: Path):
-    """Find the first CSV file inside the downloaded dataset directory."""
+    """Find the first CSV file inside the downloaded dataset directory.
+    
+    Args:
+        dataset_path (Path): The path to the directory where the dataset will be downloaded.
+    
+    Returns:
+        Path: The path to the CSV file.
+        None: If the file path is invalid or does not exist.
+    """
     csv_files = list(dataset_path.glob("*.csv"))
     if not csv_files:
         logger.error("⚠️ No CSV files found in dataset path: %s", dataset_path)
@@ -36,11 +53,19 @@ def find_first_csv(dataset_path: Path):
     return csv_files[0]
 
 def extract_data_csv(file_path: Path):
-    """Extract data from CSV file."""
+    """Extract data from CSV file.
+    
+    Args:
+        file_path (Path): The path to the CSV file.
+    
+    Returns:
+        pd.DataFrame: DataFrame from the CSV file.
+        None: If the file path is invalid or does not exist.
+    """
 
     # Step 1 : Check if the file path is valid and exists
     # Check if the file path is valid
-    if not file_path.suffix == ('.csv'):
+    if file_path.suffix != '.csv':
         logger.error("❌ File path does not end with CSV Formats")
         return None
 
@@ -78,7 +103,17 @@ def extract_data_csv(file_path: Path):
         logger.error(f"❌ Another Error to read CSV file: {str(e)}")
         return None
 
-def get_data(project_root: Path):
+def extract_get_data(project_root: Path):
+    """
+    Extract all data format from CSV file.
+    
+    Args:
+        project_root (Path): The path to the root directory of the project.
+    
+    Returns:
+        pd.DataFrame: DataFrame from the CSV file.
+        None: If the file path is invalid or does not exist.
+    """
     print("="*60)
     print("PHASE 1: EXTRACT CSV FORMATS")
     print("="*60)
@@ -98,23 +133,31 @@ def get_data(project_root: Path):
             dataset_path = download_dataset("carrie1/ecommerce-data", output_dir=data_dir, force_download=True)
         csv_path = find_first_csv(dataset_path)
 
-    # Check formats .csv and the data exists
-    if not csv_path is None and csv_path.suffix == ".csv" and csv_path.exists():
+    # Check the data exists
+    if csv_path:
         df = extract_data_csv(csv_path)
         logger.info("✔ EXTRACT AND READ CSV FILES IS SUCCESSFULLY")
         return df
     else:
-        csv_name = csv_path.name if csv_path else "CSV File"
-        logger.error(f"❌ File {csv_name} does not exist or is not valid")
+        logger.error(f"❌ File {csv_path.name} does not exist or is not valid")
         return None
 
 def summary_data(dataframe: pd.DataFrame):
+    """
+    Summary data from DataFrame.
+
+    Args:
+        dataframe (pd.DataFrame): DataFrame from the CSV file.
+    
+    Returns:
+        None: If the file path is invalid or does not exist.
+    """
     data = dataframe
 
     # Check if Dataframe is None
     if data is None:
         logger.error("❌ Data is Cannot Summarize")
-        return
+        return None
 
     print("\n")
     print("="*60)
@@ -137,7 +180,7 @@ if __name__ == "__main__":
     project_root = Path(__file__).resolve().parent.parent
 
     # Final Result From Extract
-    result_data = get_data(project_root)
+    result_data = extract_get_data(project_root)
 
     # Function For Summary Data
     summary_data(result_data)
